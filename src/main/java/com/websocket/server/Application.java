cac52a80f181753944087a35422097ef7d15c6cb
huyle <ghuy102@gmail.com>
2024-12-08 11:58:15 +0700

    Test regex for request line

--- src/main/java/com/websocket/server/Application.java
+++ src/main/java/com/websocket/server/Application.java
@@ -1,9 +1,17 @@
 package com.websocket.server;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Start the application");
-	}
+//		new WebsocketServer().start();
+		Pattern pattern = Pattern.compile("GET (((/[a-zA-Z]+)+/?)|(/[a-zA-Z]*)) HTTP/(1\\.1|2\\.0|3\\.0)");
+		Matcher matcher = pattern.matcher("GET / HTTP/2.0");
+		if (matcher.matches()) {
+			System.out.println("Find");
+		} else System.out.println("Not found");
 
+	}
 }
